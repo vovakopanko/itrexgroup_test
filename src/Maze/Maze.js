52ffa7f47162cd783914a4213647718aaaa6e783
vovakopanko <vovakopanko@mail.ru>
2021-06-25 13:14:26 +0300

    render maze for customer

--- src/Maze/Maze.js
+++ src/Maze/Maze.js
@@ -1,58 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+
+const findPath = (maze, point, result) => {
+  maze[point.x][point.y] = "visited"; // mark current point as visited
+
+  // check if we've reached exit
+  if (
+    point.x === 0 ||
+    point.y === 0 ||
+    point.x + 1 === maze.length ||
+    point.y + 1 === maze[point.x].length
+  ) {
+    return true;
+  }
+
+  // define queue of way options
+  const currentQueue = [];
+
+  // check every direction:
+
+  if (maze[point.x + 1][point.y] === "+") {
+    currentQueue.push({ x: point.x + 1, y: point.y, direction: "bottom" });
+  }
+
+  if (maze[point.x - 1][point.y] === "+") {
+    currentQueue.push({ x: point.x - 1, y: point.y, direction: "top" });
+  }
+
+  if (maze[point.x][point.y + 1] === "+") {
+    currentQueue.push({ x: point.x, y: point.y + 1, direction: "right" });
+  }
+
+  if (maze[point.x][point.y - 1] === "+") {
+    currentQueue.push({ x: point.x, y: point.y - 1, direction: "left" });
+  }
+
+  let wayOutFound; //Scope
+
+  // run through queue for current point
+  currentQueue.forEach((point) => {
+    result.push(point.direction);
+    wayOutFound = findPath(maze, point, result);
+
+    // if we got stuck then remove last direction from paths array
+    if (!wayOutFound) {
+      result.pop();
+    }
+  });
+
+  return wayOutFound;
+};
 
 const Maze = () => {
-  const findPath = (maze, point, result) => {
-    maze[point.x][point.y] = "visited"; // mark current point as visited
-
-    // check if we've reached exit
-    if (
-      point.x === 0 ||
-      point.y === 0 ||
-      point.x + 1 === maze.length ||
-      point.y + 1 === maze[point.x].length
-    ) {
-      return "done";
-    }
-
-    // define queue of way options
-    const currentQueue = [];
-
-    // check every direction:
-
-    if (maze[point.x + 1][point.y] === "+") {
-      currentQueue.push({ x: point.x + 1, y: point.y, direction: "bottom" });
-    }
-
-    if (maze[point.x - 1][point.y] === "+") {
-      currentQueue.push({ x: point.x - 1, y: point.y, direction: "top" });
-    }
-
-    if (maze[point.x][point.y + 1] === "+") {
-      currentQueue.push({ x: point.x, y: point.y + 1, direction: "right" });
-    }
-
-    if (maze[point.x][point.y - 1] === "+") {
-      currentQueue.push({ x: point.x, y: point.y - 1, direction: "left" });
-    }
-
-    let latestResult; //Scope
-
-    // run through queue for current point
-    currentQueue.forEach((point) => {
-      result.push(point.direction);
-      latestResult = findPath(maze, point, result);
-
-      // if we got stuck then remove last direction from paths array
-      if (latestResult !== "done") {
-        result.pop();
-      }
-    });
-
-    return latestResult;
-  };
-
-
-  const maze = [
+  // todo: setMaze from user input
+  const [maze, setMaze] = useState([
     ["#", "#", "#", "#", "#", "#", "#", "#", "#"],
     ["#", "+", "+", "+", "#", "+", "+", "+", "#"],
     ["#", "+", "#", "+", "#", "+", "#", "+", "#"],
@@ -61,7 +61,11 @@ const Maze = () => {
     ["#", "#", "+", "+", "#", "#", "#", "#", "#"],
     ["#", "#", "+", "#", "#", "#", "#", "#", "#"],
     ["#", "#", "#", "#", "#", "#", "#", "#", "#"],
-  ];
+  ]);
+
+  const copy = maze.map((row) => {
+    return row.slice();
+  });
 
   let startPoint;
 
@@ -72,16 +76,43 @@ const Maze = () => {
   }
 
   const path = [];
-  findPath(maze, startPoint, path);
+  findPath(copy, startPoint, path);
 
   return (
-    <div>
+    <div
+      style={{
+        paddingTop: "100px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <table>
+        {maze.map((row, i) => {
+          return (
+            <tr key={i} style={{ fontSize: "20px", fontWeight: "bold" }}>
+              {row.map((column, j) => {
+                return (
+                  <td
+                    key={j}
+                    style={{ padding: "5px", border: "1px solid black" }}
+                  >
+                    {column}{" "}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </table>
       <div>
-        <p>The way out from Maze:</p>
+        {path && <span>The way out from Maze:</span>}
+        {!path && <span>There is no way out from Maze</span>}
       </div>
       <div>
         {path.map((m) => (
-          <span>{m + " "}</span>
+          <span style={{ fontWeight: "bold" }}>{m + " "}</span>
         ))}
       </div>
     </div>
